ab931943d9141915685523d5acd2f84cbff51212
truco23 <trucopaulista23@gmail.com>
2019-05-17 17:13:52 -0300

    Adicionando sass no projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+const gulp          = require('gulp');
+const browserSync   = require('browser-sync').create();
+const sass          = require('gulp-sass');
 
 const src = {
-    index: './src/'
+    index: './src/',
+    scss: './src/scss/**/*.scss',
+    tmpCss: './src/_tmp/css'
 };
 
 const watch = {
-    base: './src/**/*'
+    base: './src/**/*',
+    scss: src.scss,
 };
 
-gulp.task('server', function() {
+gulp.task('sass', function () {
+    return gulp.src(src.scss)
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest(src.tmpCss));
+});
+
+gulp.task('server', ['compile'], function() {
 
     browserSync.init({
         server: src.index
     });
 
+    gulp.watch([watch.scss], ['sass']);
     gulp.watch(watch.base).on('change', browserSync.reload);
 });
 
+gulp.task('compile', ['sass']);
 gulp.task('dev', ['server']);
